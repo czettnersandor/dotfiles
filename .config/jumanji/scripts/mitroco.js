4de3afb37282dcad27989ddc63b9741d120adc54
Sándor Czettner <sandor@czettner.hu>
2015-01-18 17:35:12 +0000

    Better zshrc

---
+++ .config/jumanji/scripts/mitroco.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name           Mitro.co userscript
+// @namespace      https://github.com/czettnersandor/mitroco-userscript
+// @version        0.0.3
+// @description    User script to use mitro.co in browsers with userscript support, for example dwb, jumanji
+// @run-at         document-end
+// ==/UserScript==
+
+//alert('this is mitroco wrapper');
